f8eb013862d0a768cff5f4a50bae57d27bfefd3e
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2019-11-05 19:03:23 +0300

    Returned global scss files such as vars, mixins and common

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //Entry point file
 // Common utils import
-import './assets/fonts/fonts.scss'
+import './common.scss'
 //Import all pages here
-import './pages/index'
+import './pages/index/index'
+import './uikit/colors-and-type/colors-and-type'
