38ad9099f1947a7a7da2570d6386a92bc147f2e0
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2019-11-16 16:26:51 +0300

    Sizing is changed from px to rem

--- src/uikit/colors-and-type/colors-and-type.js
+++ src/uikit/colors-and-type/colors-and-type.js
@@ -1,3 +1,4 @@
 import '../../components/toxin-icon/toxin-icon'
 import '../../components/color-block/color-block'
+import '../../components/labeled-type/labeled-type'
 import './colors-and-type.scss'
