b07cc0c670839a3ce8481a7dc55702b5fd9ddfa2
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2019-11-29 16:26:19 +0300

    Toxin icon now is in uikit-template.pug

--- src/uikit/colors-and-type/colors-and-type.js
+++ src/uikit/colors-and-type/colors-and-type.js
@@ -1,4 +1,3 @@
-import '../../components/toxin-icon/toxin-icon'
 import '../../components/color-block/color-block'
 import '../../components/labeled-type/labeled-type'
 import './colors-and-type.scss'
