319e38e5f3105c1a491451106b492fb3800b0f2a
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2020-01-21 20:28:19 +0300

    Datepicker block finished

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,30 +1,16 @@
 import '../text-field/text-field'
 import './dropdown.scss'
+import './dropdown--dates'
 
 $(document).ready(function(){
-  $('.text-field--range-date').datepicker({clearButton: true, applyButton: true, range: true, multipleDatesSeparator: ' - ', dateFormat: 'dd M', inline: true});
-  $('.text-field--separated-date').datepicker({clearButton: true, applyButton: true, showEvent: 'click', range: true, twoInputsIdDiff: 'checkin checkout', dateFormat: 'dd.mm.yyyy'});
-  $('.text-field--separated-date').each(function() {
-    var _this = $(this);
-    var datepicker = _this.data('datepicker')
-      _this.parent().on('mouseup', function (e) {
-        e.originalEvent.inFocus = true;
-        _this.off('blur');
-      });
-      datepicker.$secondEl.on('click', function(e) {
-        if (datepicker.visible) {
-          datepicker.hide();
-        }
-        else {
-          datepicker.show();
-        }
-      })
-      datepicker.$secondEl.on('mouseup', function(e) {
-        e.originalEvent.inFocus = true;
-      })
-      datepicker.$secondEl.parent().on('mouseup', function(e) {
-        e.stopPropagation();
-      })
+  $('.text-field--dropdown').keypress(function(event) {
+    if (event.keyCode === 13) {
+      $(this).click()
+    }
+  });
+  $('.dropdown').on('mouseup', function(e) {
+    e.originalEvent.inFocus = true;
+    $(this).children().off('blur');
   });
 })
 
