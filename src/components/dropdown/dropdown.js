7bdf1ca5215682cebd7addda28fcfe39aa9ed5a4
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2020-02-21 00:45:45 +0300

    dropdown arrow pointer-events disabled

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,16 +2,13 @@ import '../text-field/text-field'
 import './dropdown.scss'
 import './dropdown--range-date'
 import './dropdown--separated-date'
+import './dropdown--room-features'
 
 $(document).ready(function(){
   $('.dropdown').children('input').keypress(function(event) {
     if (event.keyCode === 13) {
       $(this).click()
     }
-  });
-  $('.dropdown').on('mouseup', function(e) {
-    e.originalEvent.inFocus = true;
-    $(this).children().off('blur');
   });
 })
 
