9e26f08993e9a29f12950f88957cb9b5c70e3945
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2020-07-09 20:10:15 +0300

    Merge conflicts solving

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -6,20 +6,7 @@ import './dropdown--room-features'
 import './dropdown--guests'
 
 $(document).ready(function(){
-<<<<<<< HEAD
   $('.dropdown').children('input').keypress(function(event) {
-=======
-  $('.text-field--range-date').datepicker({clearButton: true, applyButton: true, showEvent: 'click', range: true, multipleDatesSeparator: ' - ', dateFormat: 'dd M', inline: true});
-  $('.text-field--separated-date').datepicker({clearButton: true, applyButton: true, showEvent: 'click', range: true, twoInputsIdDiff: 'checkin checkout', dateFormat: 'dd.mm.yyyy'});
-  $('.text-field--range-date, .text-field--separated-date').datepicker({
-    onShow: function(inst, animationCompleted){
-      inst.inputs.map(input => input.parent().addClass('dropdown--active'))},
-    onHide: function(inst, animationCompleted) {
-      inst.inputs.map(input => input.parent().removeClass('dropdown--active'))
-    }
-  })
-  $('.text-field--dropdown').keypress(function(event) {
->>>>>>> Datepicker block finished
     if (event.keyCode === 13) {
       $(this).click()
     }
